1044f03b3fe6c12cf30fbb46729506f951091ee7
李鸣 <MING.LI05@hand-china.com>
2021-12-13 17:30:10 +0800

    [ADD] jenkins测试

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>Learn Nginx</div>
-        <div>Hello World 111</div>
+        <div>Hello World 111222</div>
       </header>
     </div>
   );
