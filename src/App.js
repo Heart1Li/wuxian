30d15e3215fb97bf1b0603a06983f8f59772c9d3
liming <1251509887@qq.com>
2021-12-14 20:48:23 +0800

    [FIX] test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-
+import img from './assets/img.jpg'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Hello World</div>
+        <div>人类高质量男性征友</div>
+        <img src={img} className="img-container"></img>
       </header>
     </div>
   );
