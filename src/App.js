6d70303f7e2aca0e855758d64558a928c31dc3df
liming <1251509887@qq.com>
2021-12-14 20:16:19 +0800

    [FIX] hello world

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>Learn Nginx</div>
-        <div>Hello World 11122233</div>
+        <div>Hello World</div>
       </header>
     </div>
   );
